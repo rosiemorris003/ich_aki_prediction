076e638a6217eed067a53528fbaebeda440976ce
rosiemorris003 <rosiemorris003@gmail.com>
2026-09-21 14:56:26 +0100

    some comment changes and minor fixes

--- sql/create_features.sql
+++ sql/create_features.sql
@@ -34,23 +34,23 @@ WHERE itemid = 51279 AND valuenum BETWEEN 1 AND 8 GROUP BY subject_id, hadm_id;
 CREATE TABLE heart_rate AS
 SELECT subject_id, hadm_id, AVG(valuenum) AS heart_rate_mean, MAX(valuenum) AS heart_rate_max
 FROM ICH_ChartEvents_24h 
-WHERE itemid = 220045 AND valuenum BETWEEN 20 AND 250 GROUP BY subject_id, hadm_id;
+WHERE itemid = 220045 AND valuenum BETWEEN 10 AND 300 GROUP BY subject_id, hadm_id;
 
 CREATE TABLE sbp AS
 SELECT subject_id, hadm_id, AVG(valuenum) AS sbp_mean, MIN(valuenum) AS sbp_min, MAX(valuenum) AS sbp_max
 FROM ICH_ChartEvents_24h 
-WHERE itemid = 220179 AND valuenum BETWEEN 30 AND 300 GROUP BY subject_id, hadm_id;
+WHERE itemid = 220179 AND valuenum BETWEEN 20 AND 350 GROUP BY subject_id, hadm_id;
 
 CREATE TABLE dbp AS
 SELECT subject_id, hadm_id, AVG(valuenum) AS dbp_mean, MIN(valuenum) AS dbp_min, MAX(valuenum) AS dbp_max
 FROM ICH_ChartEvents_24h 
-WHERE itemid = 220180 AND valuenum BETWEEN 10 AND 200 GROUP BY subject_id, hadm_id;
+WHERE itemid = 220180 AND valuenum BETWEEN 5 AND 250 GROUP BY subject_id, hadm_id;
 
 --convert farenheit temp to celsius and remove unrealistic values
 CREATE TABLE temperature_clean AS
 SELECT subject_id, hadm_id, charttime, valuenum AS temp_c
 FROM ICH_ChartEvents_24h 
-WHERE itemid = 223762 AND valuenum BETWEEN 30 AND 45;
+WHERE itemid = 223762 AND valuenum BETWEEN 20 AND 50;
 
 INSERT INTO temperature_clean
 SELECT subject_id, hadm_id, charttime, (valuenum - 32) * 5.0 / 9.0 AS temp_c
@@ -61,7 +61,7 @@ WHERE itemid = 223761 AND valuenum IS NOT NULL;
 CREATE TABLE temperature AS
 SELECT subject_id, hadm_id, AVG(temp_c) AS temperature_mean, MIN(temp_c) AS temperature_min
 FROM temperature_clean 
-WHERE temp_c BETWEEN 30 AND 45 GROUP BY subject_id, hadm_id;
+WHERE temp_c BETWEEN 20 AND 50 GROUP BY subject_id, hadm_id;
 
 --make the weight table using the input events table with the column patient weight
 CREATE TABLE weight AS
@@ -71,7 +71,7 @@ JOIN InputEvents i
     ON f.subject_id = i.subject_id
    AND f.hadm_id = i.hadm_id
    AND f.stay_id = i.stay_id
-WHERE i.patientweight BETWEEN 30 AND 300
+WHERE i.patientweight BETWEEN 20 AND 400
   AND datetime(i.starttime) >= datetime(f.intime)
   AND datetime(i.starttime) < datetime(f.intime, '+24 hours')
 GROUP BY f.subject_id, f.hadm_id;
